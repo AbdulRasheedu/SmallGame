4c6e0d62d8caee2f2768950d2d64d1712db8137c
AbdulRasheedu <131258568+AbdulRasheedu@users.noreply.github.com>
2024-07-25 23:37:27 +0530

    "commit"

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 function bat(){
-    console.log('You Chosen Bat');
-    computer_choice = Math.round(Math.random()*3);
-    if(computer_choice > 0 && computer_choice<=1){
-        console.log('computer chosen bat');
+    console.log('You hava Chosen Bat');
+    let computerChoice;
+    let random_Number = Math.round(Math.random()*3);
+    if(random_Number >0 && random_Number <=1){
+        computerChoice = 'Bat';
+        console.log('Computer Choice is Bat');
     }
-    else if(computer_choice <=2){
-        console.log('computer chosen ball');
+    else if(random_Number >1 && random_Number <=2){
+        computerChoice = 'Bat';
+        console.log('Computer Choice is Ball');
     }
     else{
-        console.log('computer chosen stump');
+        computerChoice = 'Stump';
+        console.log('Computer Choice is Stump');
     }
-    // alert(computer_choice);
 }
 
+function ball(){
+    console.log('You hava Chosen Ball');
+    let computerChoice;
+    let random_Number = Math.round(Math.random()*3);
+    if(random_Number >0 && random_Number <=1){
+        computerChoice = 'Bat';
+        console.log('Computer Choice is Bat');
+    }
+    else if(random_Number >1 && random_Number <=2){
+        computerChoice = 'Bat';
+        console.log('Computer Choice is Ball');
+    }
+    else{
+        computerChoice = 'Stump';
+        console.log('Computer Choice is Stump');
+    }
+}
 
-
- 
+function stump(){
+    console.log('You hava Chosen Stump');
+    let computerChoice;
+    let random_Number = Math.round(Math.random()*3);
+    if(random_Number >0 && random_Number <=1){
+        computerChoice = 'Bat';
+        console.log('Computer Choice is Bat');
+    }
+    else if(random_Number >1 && random_Number <=2){
+        computerChoice = 'Bat';
+        console.log('Computer Choice is Ball');
+    }
+    else{
+        computerChoice = 'Stump';
+        console.log('Computer Choice is Stump');
+    }
+} 
 
   
