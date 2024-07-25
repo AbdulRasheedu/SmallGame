41be63c4cc3a194d7654c4c940fd25dc84eb5a43
AbdulRasheedu <131258568+AbdulRasheedu@users.noreply.github.com>
2024-07-25 15:46:39 +0530

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let computer_choice = Math.round(Math.random()*3);
+var Computer_result;
+var User_Choice;
+var result;
+if(computer_choice <=1){
+    Computer_result = "Bat"; 
+}
+else if(computer_choice <=2){
+    Computer_result = "Ball";
+}else{
+    Computer_result = "Stump";
+}
+
+
+
+ 
+
+  
