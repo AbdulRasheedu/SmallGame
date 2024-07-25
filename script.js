9ef30185da4dbb44bcca1a1b029cfad39400b0eb
AbdulRasheedu <131258568+AbdulRasheedu@users.noreply.github.com>
2024-07-25 18:20:54 +0530

    "commit"

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-
+function bat(){
+    console.log('You Chosen Bat');
+    computer_choice = Math.round(Math.random()*3);
+    if(computer_choice > 0 && computer_choice<=1){
+        console.log('computer chosen bat');
+    }
+    else if(computer_choice <=2){
+        console.log('computer chosen ball');
+    }
+    else{
+        console.log('computer chosen stump');
+    }
+    // alert(computer_choice);
+}
 
 
 
