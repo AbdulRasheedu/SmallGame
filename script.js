7540633181b62febbdd35102a84ccefe86db7ff8
AbdulRasheedu <131258568+AbdulRasheedu@users.noreply.github.com>
2024-07-26 16:14:11 +0530

    'commit'

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
 function randomNumber() {
-    let random_Number = Math.floor(Math.random() * 3); // Generates a number between 0 and 2
-    if (random_Number === 0) {
-        return 'Bat';
-    } else if (random_Number === 1) {
-        return 'Ball';
-    } else {
-        return 'Stump';
-    }
+  let random_Number = Math.floor(Math.random() * 3); // Generates a number between 0 and 2
+  if (random_Number === 0) {
+    return "Bat";
+  } else if (random_Number === 1) {
+    return "Ball";
+  } else {
+    return "Stump";
+  }
 }
 
 function bat(computerChoice) {
-    if (computerChoice === "Bat") {
-        return "Game has Tie";
-    } else if (computerChoice === "Ball") {
-        return "You Have Won The Game";
-    } else {
-        return "Computer Has Won The Game";
-    }
+  if (computerChoice === "Bat") {
+    return "Game has Tie";
+  } else if (computerChoice === "Ball") {
+    return "You Have Won The Game";
+  } else {
+    return "Computer Has Won The Game";
+  }
 }
 
 function ball(computerChoice) {
-    if (computerChoice === "Ball") {
-        return "Game has Tie";
-    } else if (computerChoice === "Stump") {
-        return "You Have Won The Game";
-    } else {
-        return "Computer Has Won The Game";
-    }
+  if (computerChoice === "Ball") {
+    return "Game has Tie";
+  } else if (computerChoice === "Stump") {
+    return "You Have Won The Game";
+  } else {
+    return "Computer Has Won The Game";
+  }
 }
 
 function stump(computerChoice) {
-    if (computerChoice === "Stump") {
-        return "Game has Tie";
-    } else if (computerChoice === "Bat") {
-        return "You Have Won The Game";
-    } else {
-        return "Computer Has Won The Game";
-    }
+  if (computerChoice === "Stump") {
+    return "Game has Tie";
+  } else if (computerChoice === "Bat") {
+    return "You Have Won The Game";
+  } else {
+    return "Computer Has Won The Game";
+  }
 }
 
 function playGame(userChoice) {
-    let computerChoice = randomNumber();
-    let result;
+  let computerChoice = randomNumber();
+  let result;
 
-    if (userChoice === "Bat") {
-        result = bat(computerChoice);
-    } else if (userChoice === "Ball") {
-        result = ball(computerChoice);
-    } else if (userChoice === "Stump") {
-        result = stump(computerChoice);
-    }
+  if (userChoice === "Bat") {
+    result = bat(computerChoice);
+  } else if (userChoice === "Ball") {
+    result = ball(computerChoice);
+  } else if (userChoice === "Stump") {
+    result = stump(computerChoice);
+  }
 
-    alert(`You Have Chosen ${userChoice}. Computer Chose ${computerChoice}. ${result}`);
+  // alert(`You Have Chosen ${userChoice}. Computer Chose ${computerChoice}. ${result}`);
+
+  let Display = document.getElementById("Display");
+  Display.innerHTML = `You Have Chosen ${userChoice}. Computer Chose ${computerChoice}. ${result}`;
 }
